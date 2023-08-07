fc45495bfe3f65c0859481b2199fd555d95e0ef3
kaizen <kaizen.step.011@gmail.com>
2023-08-07 17:43:06 +0330

    second commit

---
+++ pages/4_📑_Conclusion.py
@@ -0,0 +1,25 @@
+# Libraries
+import streamlit as st
+from PIL import Image
+
+# Layout
+st.set_page_config(page_title='Blend Protocol On-Chain Activity',
+                   page_icon=':bar_chart:📈', layout='wide')
+st.title(' Conclusion 📑 ')
+
+
+st.info(""" 
+Blur has been the most important player in the NFT ecosystem for the past three to six months, and they have recently made some big moves. They launched Blend, a product that allows users to get an NFT by paying a small fraction of the total cost upfront, and they will find a lender who will pay the rest. They knew right out of the gate what it would look like, and this has had a big impact on the market behavior. Some Twitter users are saying that lending and NFTs are a bad combination. Still, we believe that the innovative lending solutions that companies like Blur are offering will drive the NFT market to new heights.  
+In terms of notable sales, we saw a few demgods sales, but everything did less than 10 ETH. The volume of sales was relatively low. We also saw some wraped crypto prunck sell for 51 ETH, and Milady range for 6 ETH, and Azuki most expesive NFT sold for 16 ETH.  
+The NFT market experienced a rapid rise in prices due to the new liquidity brought in by token buying, which allowed airdrop farmers to bid up NFT prices. However, this resulted in one of the sharpest downturns in NFT history, which lasted for three months. This article will explore whether this trend will repeat in the future and provide insight into what factors led to the previous downturn.  
+Incentivized bidding, which was expected to have a positive impact on prices, led to a negative impact over a three-month period. One of the main reasons was a shift in supply from long-term holders to flippers, who needed to get their money back quickly. This caused a perception of oversupply and kept real buyers away, leading to a decline in prices. Moreover, momentum assets like NFTs can experience a price drop when the ball starts rolling.
+The complex process of airdrop farming and earning blur left many average users out of the market, as they saw some pump and dumps and could not understand what was going on. Therefore, they opted to wait for things to be over. This contributed to the negativity that followed.
+The question is whether the NFT market will experience a similar trend in the future, where prices go up and then decline. While there are similarities, there are also some differences that could make this different.  
+One of the things that is certain is that we need to expect volatility in both directions, as people can get leverage and pump the price up higher, or the downside can accelerate due to liquidation. However, unlike the previous trend, buyers who buy these projects are the ones who really want to buy that project, which creates a perception of more demand.
+The NFT market has been experiencing significant growth, with volumes going up by just a small bit. The top three players were rap crypto punks, azuki's Milady maker, and a couple of other projects. People are anticipating that these projects will get lending on Blur sooner rather than later. Market share largely with Blur went from about 70% to 75%. Unique users were actually down, and a lot of people are still trying to figure out what is going on.  
+Overall, Blur's recent moves have had a big impact on the NFT market. The lending solutions they are offering are innovative and will drive the market to new heights. While some people may be hesitant about the combination of lending and NFTs, we believe that these new solutions will be a game-changer for the NFT ecosystem.the NFT market is expected to experience volatility in both directions, but buyers who like the projects will be doing the buying, which may create more demand.  
+
+""")
+
+st.warning(""" 
+It's important to note that borrowing ETH comes with risks, as the value of cryptocurrencies can be volatile and subject to market fluctuations. However, if you're confident in your ability to repay the loan and believe that the value of your NFTs will appreciate over time, borrowing ETH on Blur can be a smart way to access liquidity and build your NFT collection. """)
